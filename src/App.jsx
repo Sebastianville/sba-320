18e2107d845f732da14cbd3862b2c300adc32b50
Sebastianville <sebastian.villegas2623@gmail.com>
2025-03-09 13:49:25 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+//! This is what must be pass: two paramters in addition to the API key https://developer.marvel.com/documentation/authorization
+
+//! the only HTTP Verb is currently accepts is GET requests: https://developer.marvel.com/documentation/generalinfo
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
